aa93d946bb6732b2103e491d7e81435c155d8038
LorezoParis1996 <lorenzoparis86@live.it>
2024-05-10 16:23:25 +0200

    aggiunti commenti nel codice

--- js/myscript.js
+++ js/myscript.js
@@ -1,6 +1,11 @@
 /* creo il ciclo for e lo testo*/
+
 for (let n = 1; n <= 100; n++) {
+    /*qui vengono aggiunte le condizioni da rispettare dentro il ciclo*/
+
     if (n % 15 == 0) {
+
+        /*qui viene stampato in console la richiesta desiderata*/
         console.log(n + ' FizzBuzz');
     } else if (n % 5 == 0) {
         console.log(n + ' Buzz');
