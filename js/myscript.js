bfbcc004e41b5be09ada3b2985883f5e9f504e70
LorezoParis1996 <lorenzoparis86@live.it>
2024-05-10 16:16:48 +0200

    creata e testata logica terzo if statement

--- js/myscript.js
+++ js/myscript.js
@@ -4,5 +4,7 @@ for (let n = 1; n <= 100; n++) {
         console.log(n + ' FizzBuzz');
     } else if (n % 5 == 0) {
         console.log(n + ' Buzz');
+    } else if (n % 3 == 0) {
+        console.log(n + ' Fizz');
     }
 }
